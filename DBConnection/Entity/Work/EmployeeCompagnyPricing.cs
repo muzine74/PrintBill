e0bfc2d5c87828ef0098ba8cecc03a0ac494eaff
muzine74 <dur_7@hotmail.com>
2026-02-02 18:47:51 -0500

    Add EmployeeCompagnyPricing

---
+++ DBConnection/Entity/Work/EmployeeCompagnyPricing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBConnection.Entity
+{
+    public class EmployeeCompagnyPricing
+    {
+        public Guid EmployeeCompagnyPricingId { get; set; }
+        public Guid CompanyPricingCalendarId { get; set; }
+        public Guid EmployeeId { get; set; }
+        public decimal EmplyeePaiment { get; set; }
+        public bool IsActive { get; set; }  = false;
+        DateTime ApplicatedDate { get; set; } = DateTime.Now;
+    }
+}
+
+
+//ecole
